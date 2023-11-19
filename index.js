8d570124be182acd9f20193495182b439f431dd5
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-11-19 20:01:31 +0500

    New Commit

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from "body-parser";
+import morgan from "morgan"; // Import morgan for logging
 
 //components
 import Connection from "./database/db.js";
 import Router from "./routes/route.js";
 
 dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev")); // Use morgan for logging before defining routes
 app.use("/", Router);
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Sufian Mustafa Portfolio (Backend) ");
+  res.send("Welcome to blogApp ");
 });
+
 const PORT = 8000;
 
 Connection();
