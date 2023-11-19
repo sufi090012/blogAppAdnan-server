dba7d46054e73086e689be4c139e46459b01f3ed
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-11-19 22:17:21 +0500

    New Commit

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ import Router from "./routes/route.js";
 dotenv.config();
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  // Allow requests from your Netlify frontend domain
+  app.use(
+    cors({
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true, // Allow credentials such as cookies
+    })
+  );
+
+  // Serve static files from the build folder
+  app.use(express.static("FrontEnd/build", { maxAge: "1d" }));
+
+  // Handle routes for your frontend app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "FrontEnd", "build", "index.html"));
+  });
+} else {
+  // In development mode, you might want to enable CORS for local testing
+  app.use(cors());
+}
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +45,7 @@ const port = process.env.PORT || 5000;
 
 mongoose
   .connect(
-    "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/blogging_App_2?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority"
+    "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/BlogApp_1?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(port, () => console.log(`Server running on port ${port}`));
